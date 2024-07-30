c81796965c346d312378550bf7bb2d097af68750
Giovanni <me@giovannibono.com>
2024-07-30 22:30:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var Utente = /** @class */ (function () {
+    function Utente(nome, cognome, email, metodoPagamento) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.metodoPagamento = metodoPagamento;
+    }
+    Utente.prototype.prenotaMezzo = function (mezzo) {
+        if (mezzo.stato === 'disponibile') {
+            mezzo.assegnaUtente(this);
+            console.log("".concat(this.nome, " ").concat(this.cognome, " ha prenotato un/a ").concat(mezzo.tipo));
+        }
+        else {
+            console.log("Il mezzo non è disponibile");
+        }
+    };
+    return Utente;
+}());
+var Mezzo = /** @class */ (function () {
+    function Mezzo(tipo, id) {
+        this.tipo = tipo;
+        this.id = id;
+        this.stato = 'disponibile';
+        this.utente = null;
+    }
+    ;
+    ;
+    Mezzo.prototype.assegnaUtente = function (utente) {
+        this.stato = 'inUso';
+        this.utente = utente;
+        console.log("assegato utente: ".concat(this.utente.nome));
+    };
+    return Mezzo;
+}());
+var Citta = /** @class */ (function () {
+    function Citta(nome) {
+        this.nome = nome;
+        this.mezziDisponibili = [];
+    }
+    Citta.prototype.aggiungiMezzo = function (mezzo) {
+        this.mezziDisponibili.push(mezzo);
+        console.log("aggiunto mezzo: ".concat(mezzo.tipo));
+    };
+    return Citta;
+}());
+//Test 
+var roma = new Citta('roma');
+var milano = new Citta('milano');
+var bici1 = new Mezzo('bici', 'B123');
+var monopattino1 = new Mezzo('monopattino', 'a123');
+var mario = new Utente('Mario', 'Rossi', 'mario@example.com', 'carta');
+var luca = new Utente('Luca', 'Bianchi', 'luca@examplexample.com', 'paypal');
+roma.aggiungiMezzo(bici1);
+milano.aggiungiMezzo(monopattino1);
+mario.prenotaMezzo(bici1);
+luca.prenotaMezzo(monopattino1);
---
+++ index.ts
@@ -0,0 +1,91 @@
+  interface IUtente {
+    nome: string;
+    cognome: string;
+    email: string;
+    metodoPagamento: "carta" | "paypal" | "contanti";
+    prenotaMezzo(mezzo: IMezzo): void;
+  }
+  
+  interface IMezzo {
+    tipo: 'bici' | 'scooter' | 'monopattino';
+    id: string;
+    stato: 'disponibile' | 'inUso';
+    assegnaUtente(utente: IUtente): void;
+  }
+  
+  interface ICitta {
+    nome: string;
+    mezziDisponibili: IMezzo[];
+    aggiungiMezzo(mezzo: IMezzo): void;
+  }
+  
+  class Utente implements IUtente {
+    nome: string;
+    cognome: string;
+    email: string;
+    metodoPagamento: "carta" | "paypal" | "contanti"; 
+  
+    constructor(nome: string, cognome: string, email: string, metodoPagamento: any) {
+      this.nome = nome;
+      this.cognome = cognome;
+      this.email = email;
+      this.metodoPagamento = metodoPagamento;
+    }
+  
+    prenotaMezzo(mezzo: IMezzo) {
+      if (mezzo.stato === 'disponibile') {
+        mezzo.assegnaUtente(this);
+        console.log(`${this.nome} ${this.cognome} ha prenotato un/a ${mezzo.tipo}`);
+      } else {
+        console.log("Il mezzo non è disponibile");
+      }
+    }
+  }
+  
+  class Mezzo implements IMezzo {
+    tipo: 'bici' | 'scooter' | 'monopattino';;
+    id: string;
+    stato:  'disponibile' | 'inUso';;
+    utente: IUtente | null;
+  
+    constructor(tipo: any, id: string) {
+      this.tipo = tipo;
+      this.id = id;
+      this.stato = 'disponibile';
+      this.utente = null;
+    }
+  
+    assegnaUtente(utente: IUtente) {
+      this.stato = 'inUso';
+      this.utente = utente;
+      console.log(`assegato utente: ${this.utente.nome}`)
+    }
+  }
+  
+  class Citta implements ICitta {
+    nome: string;
+    mezziDisponibili: Mezzo[];
+  
+    constructor(nome: string) {
+      this.nome = nome;
+      this.mezziDisponibili = [];
+    }
+  
+    aggiungiMezzo(mezzo: Mezzo) {
+      this.mezziDisponibili.push(mezzo);
+      console.log(`aggiunto mezzo: ${mezzo.tipo}`);
+    }
+  }
+
+//Test 
+const roma = new Citta('roma');
+const milano = new Citta('milano');
+const bici1 = new Mezzo('bici', 'B123');
+const monopattino1 = new Mezzo('monopattino', 'a123');
+const mario = new Utente('Mario', 'Rossi', 'mario@example.com', 'carta');
+const luca = new Utente('Luca', 'Bianchi', 'luca@examplexample.com', 'paypal');
+
+roma.aggiungiMezzo(bici1);
+milano.aggiungiMezzo(monopattino1);
+mario.prenotaMezzo(bici1);
+luca.prenotaMezzo(monopattino1);
